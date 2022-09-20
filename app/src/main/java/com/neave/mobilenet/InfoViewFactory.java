3553de90499ca9b2d5f3c66b639312442a48a91a
Neave Paul <me@neave.com>
2022-09-21 00:14:41 +0530

    updating classification specific classes

--- app/src/main/java/com/neave/mobilenet/InfoViewFactory.java
+++ app/src/main/java/com/neave/mobilenet/InfoViewFactory.java
@@ -10,25 +10,12 @@ import androidx.annotation.Nullable;
 import com.neave.mobilenet.R;
 
 public class InfoViewFactory {
-  public static final int INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET = 1;
   public static final int INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET = 2;
   public static final int INFO_VIEW_TYPE_TEXT_CLASSIFICATION = 3;
 
   public static View newInfoView(Context context, int infoViewType, @Nullable String additionalText) {
     LayoutInflater inflater = LayoutInflater.from(context);
-    if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET == infoViewType) {
-      View view = inflater.inflate(R.layout.info, null, false);
-      TextView infoTextView = view.findViewById(R.id.info_title);
-      TextView descriptionTextView = view.findViewById(R.id.info_description);
-
-      infoTextView.setText(R.string.vision_card_capture_title);
-      StringBuilder sb = new StringBuilder(context.getString(R.string.vision_card_capture_description));
-      if (additionalText != null) {
-        sb.append('\n').append(additionalText);
-      }
-      descriptionTextView.setText(sb.toString());
-      return view;
-    } else if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET == infoViewType) {
+    if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET == infoViewType) {
       View view = inflater.inflate(R.layout.info, null, false);
       TextView infoTextView = view.findViewById(R.id.info_title);
       TextView descriptionTextView = view.findViewById(R.id.info_description);
@@ -39,14 +26,6 @@ public class InfoViewFactory {
         sb.append('\n').append(additionalText);
       }
       descriptionTextView.setText(sb.toString());
-      return view;
-    } else if (INFO_VIEW_TYPE_TEXT_CLASSIFICATION == infoViewType) {
-      View view = inflater.inflate(R.layout.info, null, false);
-      TextView infoTextView = view.findViewById(R.id.info_title);
-      TextView descriptionTextView = view.findViewById(R.id.info_description);
-
-      infoTextView.setText(R.string.nlp_card_lstm_title);
-      descriptionTextView.setText(R.string.nlp_card_lstm_description);
       return view;
     }
     throw new IllegalArgumentException("Unknown info view type");
