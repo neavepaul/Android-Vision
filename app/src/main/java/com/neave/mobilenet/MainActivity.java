fc9cc29041b27bffe307c24c478656863dd8e68b
Neave Paul <me@neave.com>
2022-09-21 00:15:12 +0530

    update main class

--- app/src/main/java/com/neave/mobilenet/MainActivity.java
+++ app/src/main/java/com/neave/mobilenet/MainActivity.java
@@ -30,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    setContentView(R.layout.activity_main);
+    Handler handler = new Handler();
+
+    findViewById(R.id.main_vision_click_view).setOnClickListener(v -> moveFromSplash());
+    // OR
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        moveFromSplash();
+
+      }
+    }, 1500);
+  }
+
   private void moveFromSplash() {
     startActivity(new Intent(MainActivity.this, VisionListActivity.class));
     overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
